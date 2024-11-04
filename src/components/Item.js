5724106cfe387db308dd438f478bd923b5d19de6
masinde sylvester <masinde.sylvester@yahoo.com>
2024-11-04 23:03:05 +0300

    initial commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [chart, setChart] = useState(false);
+  const chartHandler = () => {
+    setChart(!chart);
+  };
+
   return (
     <li className="">
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={chartHandler} className={`${chart ? "in-cart" : ""}`}>
+        {`${chart ? "Remove From Cart" : "Add to Cart"}`}
+      </button>
     </li>
   );
 }
